080d1dce93f074259669eee9553a21642dc65b27
maximxmoroz <maximxmoroz@gmail.com>
2021-12-14 11:06:35 +0300

    sprint4

--- yatube/core/templatetags/user_filters.py
+++ yatube/core/templatetags/user_filters.py
@@ -3,6 +3,7 @@ from django import template
 # добавляем к ним и наш фильтр.
 register = template.Library()
 
+
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
